b51e07867c49ce10f9bdc9931749824b5a948df2
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-03-05 21:44:27 -0600

    Polimorfismo interface

---
+++ alcance-o-encapsulamiento/Alcance.php
@@ -0,0 +1,18 @@
+<?php
+
+class User
+{
+    // public
+    // protected
+    // private
+    public const PAGINATE = 25;
+
+    public $username;
+    // protected $username;
+    // private $username;
+
+    public function getUsername()
+    {
+        // ...
+    }
+}
